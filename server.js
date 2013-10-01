1235125de928471ea8f968b6210cdfc799e0ce7c
angelahpark <ahaekyungp@gmail.com>
2013-10-01 14:52:06 -0700

    - Revert

--- server.js
+++ server.js
@@ -6,22 +6,21 @@ var util = require('util'),
 
 var app = express();
 
-app.configure(function() {
-  app.set('port', process.env.PORT || 8000);
-  app.set('view engine', 'jade');
-  app.set('views', __dirname + '/app/views');
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.bodyParser());
-  app.use(express.cookieParser());
-  app.set('title', 'angela h park');
-  app.use(before);
-  app.use(app.router);
-});
+app.set('port', process.env.PORT || 8000);
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/app/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 var before = function(req, res, next){
 	console.log("Before request: " + req.url);
 	next();
 }
+
+app.configure(function(){
+  app.set('title', 'angela h park');
+  app.use(before);
+  app.use(app.router);
+});
 
 routes(app);
 
@@ -41,11 +40,7 @@ app.use(function(req, res, next){
   res.status(404).render('404', { url: req.originalUrl });
 });
 
-// http.createServer(app).listen(app.get('port'), function(){
-  http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-  }).listen(80, '192.241.232.72');
-
+http.createServer(app).listen(app.get('port'), function(){
+  
   console.log('Express server listening on port ' + app.get('port'));
 });
