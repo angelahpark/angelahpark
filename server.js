8ae06af541f3a316ef0703324cddb7feb8b3c73d
angelahpark <ahaekyungp@gmail.com>
2013-10-02 13:08:36 -0700

    - new style changes - slider added

--- server.js
+++ server.js
@@ -6,7 +6,15 @@ var util = require('util'),
 
 var app = express();
 
-app.set('port', process.env.PORT || 8000);
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('We\'re in \n');
+}).listen(8000);
+console.log('Server running at http://<your server ip address>:3000/');
+
+
+// app.set('port', process.env.PORT || 8000);
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/app/views');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -40,7 +48,7 @@ app.use(function(req, res, next){
   res.status(404).render('404', { url: req.originalUrl });
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+// http.createServer(app).listen(app.get('port'), function(){
   
-  console.log('Express server listening on port ' + app.get('port'));
-});
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
