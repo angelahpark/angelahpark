262542655746c9d1ea6f0b1cee1980e25620ce56
angelahpark <ahaekyungp@gmail.com>
2013-11-21 11:22:13 -0800

    - new social media icons - styling changes - added portfolio and social object

--- server.js
+++ server.js
@@ -40,6 +40,41 @@ app.use(function(req, res, next){
   res.status(404).render('404', { url: req.originalUrl });
 });
 
+
+
+
+
+// contact
+// var nodemailer = require("nodemailer");
+
+// var smtpTransport = nodemailer.createTransport("SMTP",{
+//    service: "Gmail",  // sets automatically host, port and connection security settings
+//    auth: {
+//        user: "ahaekyungp@gmail.com",
+//        pass: "Park1125"
+//    }
+// });
+
+// smtpTransport.sendMail({  //email options
+//    from: "Angela Park <ahaekyungp@gmail.com>", // sender address.  Must be the same as authenticated user if using Gmail.
+//    to: "Angela Park <ahaekyungp@gmail.com>", // receiver
+//    subject: "Emailing with nodemailer", // subject
+//    text: "Email Example with nodemailer" // body
+// }, function(error, response){  //callback
+//    if(error){
+//        console.log(error);
+//    }else{
+//        console.log("Message sent: " + response.message);
+//    }
+   
+//    smtpTransport.close(); // shut down the connection pool, no more messages.  Comment this line out to continue sending emails.
+// });
+
+
+
+
+
+
 http.createServer(app).listen(app.get('port'), function(){
   
   console.log('Express server listening on port ' + app.get('port'));
